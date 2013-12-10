32527011adc62bd6a07ed975b7bfdec1923e12b5
jemerensiana <koko_ang@yahoo.com>
2013-12-10 01:47:59 -0500

    Euler Problem - Solution
    Finding the prime factor of each numbers (1-20), then keeping record of
    the max occurrences of each prime number, then compute multiplication of
    prime to the power of occurrences.

---
+++ EP005.java
@@ -0,0 +1,58 @@
+/* Euler Prolem 005
+ * 2520 is the smallest number that can be divided by each of
+ * the numbers from 1 to 10 without any remainder.
+ * What is the smallest positive number that is evenly divisible
+ * by all of the numbers from 1 to 20?
+ */
+
+package Euler;
+
+public class EP005 {
+
+	private long[] prime = { 2, 3, 5, 7, 11, 13, 17, 19 };
+	private long[] occurrences = { 0, 0, 0, 0, 0, 0, 0, 0 };
+	private long res = 1;
+
+	public void compute(long number) {
+
+		long n = number;
+		long counter;
+
+		//prime factor 
+		for (int i = 0; i < prime.length; i++) {
+			counter = 0;
+			while (n % prime[i] == 0) {
+				n /= prime[i];
+				counter = counter + 1;
+			}
+			
+			if (occurrences[i] < counter)
+				occurrences[i] = counter;
+		}
+
+	}
+
+	public void solution() {
+				
+		//list of number 2-20
+		for(long i = 2; i <= 20; i++) {
+			compute(i);
+		}
+		
+		//compute the smallest positive number
+		for(int i = 0; i < prime.length; i++) {
+			res *= (long) Math.pow((double)prime[i], (double)occurrences[i]);
+		}
+		
+		System.out.println(Long.toString(res));
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new EP005().solution();
+	}
+
+}
